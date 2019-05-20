81a4734de48b7f19cdc79d3e9b871fa91474066b
Shreya Dahal <shreyadahal@gmail.com>
2019-05-20 10:16:29 +0545

    Print basic game board

--- libminesweeper/minesweeper.c
+++ libminesweeper/minesweeper.c
@@ -8,9 +8,9 @@ int random_number(int max) {
   return rand() % max;
 }
 
-int array_has_value(int *arr, int val) {
+int array_has_value(int *arr, int arr_size, int val) {
   int i;
-  for (i = 0; i < sizeof(arr) / sizeof(int); i++) {
+  for (i = 0; i < arr_size; i++) {
     if (arr[i] == val) {
       return 1;
     }
@@ -26,7 +26,7 @@ void init_bomb_positions(int *bomb_positions, int total_bombs, int total_cells) 
   i = 0;
   while(1) {
     tmp = random_number(total_cells);
-    if (!array_has_value(bomb_positions, tmp)) {
+    if (!array_has_value(bomb_positions, total_bombs, tmp)) {
       bomb_positions[i] = tmp;
       i++;
     }
@@ -43,7 +43,7 @@ MinesweeperCtx* msw_init(int rows, int cols) {
   MinesweeperCtx *game = (MinesweeperCtx*) malloc(sizeof(MinesweeperCtx));
 
   total_cells = rows * cols;
-  total_bombs = total_cells * 0.1;
+  total_bombs = 15000 / total_cells; // 15% of the cells have a bomb
 
   int *bomb_positions = (int*) malloc(sizeof(int) * total_bombs);
   int *flag_positions = (int*) malloc(sizeof(int) * total_cells);
--- main.c
+++ main.c
@@ -1,21 +1,48 @@
+#include <stdlib.h>
+
 #include "window.h"
 #include "libminesweeper/minesweeper.h"
 
 int process_character(char ch, MinesweeperCtx *game);
 
-int main()
+int main(int argc, char **argv)
 {
+  int i, j;
+  int rows, cols;
   int cmd;
   MinesweeperCtx *game;
 
   window_init();
 
-  game = msw_init(16, 16);
+  if (argc != 3) {
+    rows = cols = 16;
+  } else {
+    rows = atoi(argv[1]);
+    cols = atoi(argv[2]);
+  }
+
+  game = msw_init(rows, cols);
 
   if (!game) {
     fprintf(stderr, "Could not initialize game.");
 
     return -1;
+  }
+
+  for (i = 0; i < rows; i++) {
+    for (j = 0; j < cols; j++) {
+      mvprintw(
+        i,
+        j,
+        "%s",
+        array_has_value(
+          game->bomb_positions,
+          game->bomb_count, i * rows + j
+        )
+          ? "+"
+          : "-"
+      );
+    }
   }
 
   while((cmd = process_character(getch(), game)));
