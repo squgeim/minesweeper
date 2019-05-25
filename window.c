dde28afb7f43387266cec228555215ad4fb12716
Shreya Dahal <shreyadahal@gmail.com>
2019-05-26 00:22:37 +0545

    Draw full grid

--- window.c
+++ window.c
@@ -18,12 +18,11 @@ GameWindow* window_init(int rows, int cols) {
   refresh();
 
   local_win = newwin(
-      rows + 2,
-      cols + 2,
-      (LINES - rows - 2) / 2,
-      (COLS - cols - 2) / 2
+      rows * 2 + 1,
+      cols *2 + 1,
+      (LINES - rows * 2 - 1) / 2,
+      (COLS - cols * 2 - 1) / 2
     );
-  box(local_win, 0, 0);
 
   wrefresh(local_win);
 
@@ -43,17 +42,46 @@ int window_exit() {
   return 0;
 }
 
-int window_draw_game(GameWindow *window, MinesweeperCtx *game) {
-  int i, j;
+int is_even(int num) {
+  return num % 2 == 0;
+}
 
-  for (i = 0; i < game->rows; i++) {
-    for (j = 0; j < game->cols; j++) {
-      mvwaddch(window->window, i + 1, j + 1, ACS_BLOCK);
-    }
+int window_draw_game(GameWindow *window, MinesweeperCtx *game) {
+  int i, j, cell_i, cell_j;
+
+  // Draw top row border:
+  mvwaddch(window->window, 0, 0, ACS_ULCORNER);
+  for (j = 1; j < game->cols * 2; j++) {
+    mvwaddch(window->window, 0, j, is_even(j) ? ACS_TTEE : ACS_HLINE);
+  }
+  mvwaddch(window->window, 0, j, ACS_URCORNER);
+
+  // Draw bottom row border:
+  mvwaddch(window->window, game->rows * 2, 0, ACS_LLCORNER);
+  for (j = 1; j < game->cols * 2; j++) {
+    mvwaddch(window->window, game->rows * 2, j, is_even(j) ? ACS_BTEE : ACS_HLINE);
+  }
+  mvwaddch(window->window, game->rows * 2, j, ACS_LRCORNER);
+
+  // Draw side borders:
+  for (i = 1; i < game->rows * 2; i++) {
+    mvwaddch(window->window, i, 0, is_even(i) ? ACS_LTEE: ACS_VLINE);
+    mvwaddch(window->window, i, game->cols * 2, is_even(i) ? ACS_RTEE: ACS_VLINE);
   }
 
-  printw("%d %d", i, j);
+  // Draw cells and grid:
+  for (i = 1, cell_i = 0; i < game->rows * 2; i++) {
+    for (j = 1, cell_j = 0; j < game->cols * 2; j++) {
+      if (!is_even(i)) {
+        mvwaddch(window->window, i, j, is_even(j) ? (cell_j++, ACS_VLINE) : ACS_BLOCK);
+      } else {
+        mvwaddch(window->window, i, j, is_even(j) ? ACS_PLUS : ACS_HLINE);
+      }
+    }
+    cell_i++;
+  }
 
   wrefresh(window->window);
+  refresh();
   return 0;
 }
